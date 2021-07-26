adcaa07faa3693cd1532f727629338e8b22ac5d9
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-07-26 16:46:15 +0300

    adding count

--- snakes-cafe/snakes_cafe/snakes-cafe.py
+++ snakes-cafe/snakes_cafe/snakes-cafe.py
@@ -39,22 +39,23 @@ Unicorn Tears
 menu = {'Wings': 0, 'Cookies': 0, 'Spring Rolls': 0, 'Salmon': 0, 'Steak': 0, 'Meat Tornado': 0,
         'A Literal Garden': 0, 'Ice Cream': 0, 'Cake': 0, 'Pie': 0, 'Coffee': 0, 'Tea': 0, 'Unicorn Tears': 0}
 
-print("What would you like to order?")
 
 # Put order
 
 
 def handle_order():
+    print("What would you like to order?")
+
     order = 0
     while (order != "quit"):
-        order = input()
+        order = input("> ")
         if (order == "quit"):
             break
         for item in menu:
             if order == item:
-               
+                menu[item] = menu[item]+1
                 print(
-                    f"  order of {item} have been added to your meal")
+                    f" {menu[item]} order of {item} have been added to your meal")
                 break
         else:
             print("This order is not available")
